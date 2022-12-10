57f30ecd13fe161892414213d6605cb6e91ce1a3
fehooaaa <dmarenkov2008@gmail.com>
2022-12-10 14:01:36 +0300

    userCard component created

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import userCard from './components/userCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    userCard
   }
 }
 </script>
